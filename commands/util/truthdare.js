8fabd6aa17276a35622426bc5d25e0cdf37e87f6
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-14 13:39:37 +0100

    Update truthdare.js

--- commands/util/truthdare.js
+++ commands/util/truthdare.js
@@ -6,21 +6,21 @@ var truthdare = {
 	cooldown: 5,
 	lastUsed: 0,
 	roleRequired: 'user',
+	getTruth: function (chat) {
+                    var c = Math.floor(Math.random() * lang.truthdare.truths.length);
+                    return lang.truthdare.truths[c];
+                },
+	getDare: function(chat) {
+                    var c = Math.floor(Math.random() * lang.truthdare.dares.length);
+                    return lang.truthdare.dares[c];
+                },
 	exec: function(bot, chat, data) {
 		switch(data.cmd) {
 			case 'truth':
-			getTruth: function (chat) {
-                    var c = Math.floor(Math.random() * lang.truthdare.truths.length);
-                    return lang.truthdare.truths[c];
-                };
 				bot.sendChat(utils.replaceString(lang.truthdare.truth, {user: chat.username, truth: this.getTruth()}), 30e3);
 				break;
 			
 			case 'dare':
-			 getDare: function(chat) {
-                    var c = Math.floor(Math.random() * lang.truthdare.dares.length);
-                    return lang.truthdare.dares[c];
-                };
 				bot.sendChat(utils.replaceString(lang.truthdare.dare, {user: chat.username, dare: this.getDare()}), 30e3);
 				break;
 		}
