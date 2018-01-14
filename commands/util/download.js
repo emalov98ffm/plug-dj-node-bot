0a8ef63600c03717653d6ccfbc2fe42f7f5b3178
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-14 12:55:54 +0100

    Create download.js

---
+++ commands/util/download.js
@@ -0,0 +1,17 @@
+var lang = require(process.cwd() + '/lang.json');
+var utils = require(process.cwd() + '/utils/utils');
+
+var download = {
+	commands: ['download'],
+	cooldown: 5,
+	lastUsed: 0,
+	roleRequired: 'user',
+	exec: function(bot, chat, data) {
+		            var media = bot.getMedia();
+		            var linkToSong = "https://break.tv/widget/mp3/?link=https://www.youtube.com/watch?v=" + media.cid;
+					bot.sendChat(utils.replaceString(lang.download.mpdownload, {user: chat.username, ulink: linkToSong}), 30e3);
+					}
+					
+};
+
+module.exports = download;
