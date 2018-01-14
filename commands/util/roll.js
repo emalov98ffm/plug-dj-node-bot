102b9c6ef2b96a8b16fd0ac5e4c40d04f7885c60
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-15 00:33:05 +0100

    Update roll.js

--- commands/util/roll.js
+++ commands/util/roll.js
@@ -10,11 +10,10 @@ var roll = {
 	var user = auxapi.users.getUserByID(chat.id);
 	var randomMax = data.params[1] + 1;
 					if ((isNaN(randomMax) == true) || (randomMax.length == 0)) {
-						var randomMax = 60;
+						var randomMax = 60
 					}
-					var randomispis = Math.floor((Math.random() * randomMax) + 1);
-					if(randomispis == 6)
-					{
+					var randomispis = Math.floor((Math.random() * randomMax) + 1)
+					if(randomispis == 6) {
 					bot.moveDJ(user, 1);
 	    return bot.sendChat("@" + chat.username + ", cestitam, upravo si osvojio prvo mjesto.");
 	}
@@ -22,6 +21,7 @@ var roll = {
 					{
 					bot.sendChat("@" + chat.username + ", tvoj nasumicno odabran broj je " + randomispis + ". Nemas srece. Pokusaj ponovo poslije.");
 					}
+		}
 					
 };
 
