e4297b8f23730f069fd47fa55222abe0a5b0e986
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-14 23:44:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -50,6 +50,10 @@ plugged.on(plugged.JOINED_ROOM, function _joinedRoom() {
         plugged.woot();
     });
 });
+
+plugged.on(plugged.USER_JOIN, function(user) {
+    plugged.sendChat("Dobrodosao/la " + user);
+};
 /*
 plugged.on(plugged.CHAT_DELETE, function(data) {
 	console.log(data ? JSON.stringify(data, null, 3) : 'Nada');
