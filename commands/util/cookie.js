b46059ff750e3e043c3ec3929e386afa9a599688
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-15 12:46:43 +0100

    Update cookie.js

--- commands/util/cookie.js
+++ commands/util/cookie.js
@@ -1,6 +1,6 @@
+var auxapi = require(process.cwd() + '/auxapi');
 var lang = require(process.cwd() + '/lang.json');
 var utils = require(process.cwd() + '/utils/utils');
-var auxapi = require(process.cwd() + '/auxapi');
 
 var cookie = {
 	commands: ['cookie'],
@@ -12,10 +12,27 @@ var cookie = {
                     return lang.fortunes.cookielist[c];
                 },
 	exec: function(bot, chat, data) {
-		var user2;
-	 var msg = data.params.join(' ').substr(1).split(' @');
-	 user2 = auxapi.users.getUserByUsername(msg[1]);
-	    bot.sendChat(utils.replaceString(lang.fortunes.cookie2, {user: chat.username, nameto: user2.username,  cookie: this.getCookieList()}), 30e3);
+	    var user1, user2;
+	    
+	    if (!data.params.length)
+	        return bot.sendChat(utils.replaceString(lang.general.userNotFound, {user: chat.username}));
+	       
+	    if (data.params[0].toLowerCase() == 'id') {
+	        user2 = auxapi.users.getUserByID(data.params[2]);
+	    } else {
+    	    var msg = data.params.join(' ').substr(1).split(' @');
+
+    	    user1 = auxapi.users.getUserByUsername(msg[0]);
+    	    user2 = auxapi.users.getUserByUsername(msg[1]);
+	    }
+
+	    if (!user1 || !user2)
+	        return bot.sendChat(utils.replaceString(lang.general.ghost, {user: chat.username}), 30e3);
+        
+        if (user1.id == user2.id)
+            return bot.sendChat(utils.replaceString(lang.swap.sameUser, {user: chat.username}), 30e3);
+
+        bot.sendChat(utils.replaceString(lang.fortunes.cookie2,{user1: user1.username, user2: user2.username,cookie: this.getCookieList()}));
 	}
 };
 
