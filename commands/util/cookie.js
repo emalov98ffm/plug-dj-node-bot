70b181d34cf78f86d6e248f137009c235bea23d4
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-15 12:39:07 +0100

    Update cookie.js

--- commands/util/cookie.js
+++ commands/util/cookie.js
@@ -12,8 +12,9 @@ var cookie = {
                     return lang.fortunes.cookielist[c];
                 },
 	exec: function(bot, chat, data) {
+		var user2;
 	 var msg = data.params.join(' ').substr(1).split(' @');
-	 var user2 = auxapi.users.getUserByUsername(msg);
+	 user2 = auxapi.users.getUserByUsername(msg[1]);
 	    bot.sendChat(utils.replaceString(lang.fortunes.cookie2, {user: chat.username, nameto: user2,  cookie: this.getCookieList()}), 30e3);
 	}
 };
