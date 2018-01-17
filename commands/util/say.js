894008937cb2ca20b8dad4324bf2f641ea3d25ae
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-17 21:43:20 +0100

    Update say.js

--- commands/util/say.js
+++ commands/util/say.js
@@ -9,7 +9,7 @@ var say = {
 	roleRequired: 'bouncer',
 	exec: function(bot, chat, data) {
 	var echohistory1, echohistory2;
-	var echoMessage =  data.params.split(' '), 1).join(' ');
+	var echoMessage =  data.params + 1;
 	 if (data.params.length == 0) {
                         bot.sendChat("@" + chat.username + ", tesko je ponoviti nista.");
                     } else if (data.params[0] == "!" || data.params.includes(" !")) {
