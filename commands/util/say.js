586cd034e4fc602abeffaed25bd7f0952347e246
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-17 21:23:47 +0100

    Update say.js

--- commands/util/say.js
+++ commands/util/say.js
@@ -1,6 +1,7 @@
 var lang = require(process.cwd() + '/lang.json');
 var utils = require(process.cwd() + '/utils/utils');
 
+
 var say = {
 	commands: ['say'],
 	cooldown: 5,
@@ -9,10 +10,10 @@ var say = {
 	roleRequired: 'none',
 	exec: function(bot, chat, data) {
 	var echohistory1, echohistory2;
-	var echoMessage =  data.params;
-	 if (data.params.length == 0) {
+	var echoMessage =  data.params + 1;
+	 if (echoMessage.length == 0) {
                         bot.sendChat("@" + chat.username + ", tesko je ponoviti nista.");
-                    } else if (data.params.includes("!") || data.params.includes(" !")) {
+                    } else if (echoMessage[0] == "!" || echoMessage.includes(" !")) {
                         bot.sendChat("@" + chat.username + ", pokusavas da ponovim komandu? Nista od toga, pokusaj nesto drugo.");
                     } else {
                         bot.sendChat(echoMessage);
