352b1825174da705e776a179b4cf3b67b01f47ea
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-17 18:29:50 +0100

    Update rps.js

--- commands/util/rps.js
+++ commands/util/rps.js
@@ -8,13 +8,6 @@ var rps = {
 	deleteMessage: false,
 	roleRequired: 'none',
 	exec: function(bot, chat, data) {
-	function(chat) {
-	var space = chat.msg.indexOf(' ');
-	if( (space === -1) ){
-       bot.sendChat("/me Da bi igrao rock paper scissors koristi !rps {izbor}");
-	return false;
-	  }
-	} else {
 	var choices = ["rock", "paper", "scissors", "lizard", "spock"];
     var botChoice = choices[Math.floor(Math.random()*choices.length)];
     var userChoice = data.params[1] + 1;
@@ -100,7 +93,6 @@ var rps = {
 		
 		
 		
-	}
 };
 
 module.exports = rps;
