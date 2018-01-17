0172ba8143096c6455ef9e12a091cdc5d00465ca
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-17 21:10:20 +0100

    Create say.js

---
+++ commands/util/say.js
@@ -0,0 +1,28 @@
+var lang = require(process.cwd() + '/lang.json');
+var utils = require(process.cwd() + '/utils/utils');
+
+var say = {
+	commands: ['say'],
+	cooldown: 5,
+	lastUsed: 0,
+	deleteMessage: true,
+	roleRequired: 'none',
+	exec: function(bot, chat, data) {
+	var echohistory1, echohistory2;
+	echoHistory1: [],
+    echoHistory2: [],
+	var echoMessage =  data.params + 1;
+	 if (echoMessage.length == 0) {
+                        bot.sendChat("@" + chat.username + ", tesko je ponoviti nista.");
+                    } else if (echoMessage[0] == "!" || echoMessage.includes(" !")) {
+                        bot.sendChat("@" + chat.username + ", pokusavas da ponovim komandu? Nista od toga, pokusaj nesto drugo.");
+                    } else {
+                echoHistory1.push(chat.username);
+                echoHistory2.push(echoMessage);
+                        bot.sendChat(echoMessage);
+                    }
+//
+	}
+};
+
+module.exports = say;
