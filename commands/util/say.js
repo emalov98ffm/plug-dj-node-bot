007dcfc5a6b5d3c0f643e3004980f107e2d83741
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-17 21:19:11 +0100

    Update say.js

--- commands/util/say.js
+++ commands/util/say.js
@@ -12,7 +12,7 @@ var say = {
 	var echoMessage =  data.params;
 	 if (echoMessage.length == 0) {
                         bot.sendChat("@" + chat.username + ", tesko je ponoviti nista.");
-                    } else if (echoMessage[0] == "!" || echoMessage.includes(" !")) {
+                    } else if (echoMessage.includes("!") || echoMessage.includes(" !")) {
                         bot.sendChat("@" + chat.username + ", pokusavas da ponovim komandu? Nista od toga, pokusaj nesto drugo.");
                     } else {
                         bot.sendChat(echoMessage);
