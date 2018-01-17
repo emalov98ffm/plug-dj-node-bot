90194e3c78c8c2ffa82cb9808b86b54d39af84e4
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-01-17 18:17:08 +0100

    Update rps.js

--- commands/util/rps.js
+++ commands/util/rps.js
@@ -13,53 +13,83 @@ var rps = {
 	} else {
 	var choices = ["rock", "paper", "scissors", "lizard", "spock"];
     var botChoice = choices[Math.floor(Math.random()*choices.length)];
-	var chatPrefix = "/me ";
-    var userChoice = data.params[1];
+    var userChoice = data.params[];
 	if(botChoice == userChoice){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"! It's a draw!");
-                } else if(botChoice == choices[0] && userChoice == choices[1]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[0] && userChoice == choices[2]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[0] && userChoice == choices[3]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[0] && userChoice == choices[4]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[1] && userChoice == choices[0]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[1] && userChoice == choices[2]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[1] && userChoice == choices[3]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[1] && userChoice == choices[4]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[2] && userChoice == choices[0]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[2] && userChoice == choices[1]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[2] && userChoice == choices[3]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[2] && userChoice == choices[4]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[3] && userChoice == choices[0]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[3] && userChoice == choices[1]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[3] && userChoice == choices[2]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[3] && userChoice == choices[4]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[4] && userChoice == choices[0]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[4] && userChoice == choices[1]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[4] && userChoice == choices[2]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else if(botChoice == choices[4] && userChoice == choices[3]){
-                    bot.sendChat(chatPrefix+"chose "+botChoice+"!");
-                } else {
-                    bot.sendChat(chatPrefix+"Please select between Rock, Paper, Scissors, Lizard, or Spock.");
-                }
+                   return  bot.sendChat(utils.replaceString(lang.rps.draw, {choice: botChoice, user: chat.username}), 30e3);
+                } else if (botChoice == "rock" && userChoice == "paper") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+
+                            } else if (botChoice == "rock" && userChoice == "scissors") {
+                               return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "rock" && userChoice == "lizard") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "rock" && userChoice == "spock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "paper" && userChoice == "rock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "paper" && userChoice == "scissors") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "paper" && userChoice == "lizard") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "paper" && userChoice == "spock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "scissors" && userChoice == "rock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "scissors" && userChoice == "paper") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "scissors" && userChoice == "lizard") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "scissors" && userChoice == "spock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "lizard" && userChoice == "rock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "lizard" && userChoice == "paper") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "lizard" && userChoice == "scissors") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "lizard" && userChoice == "spock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "spock" && userChoice == "rock") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "spock" && userChoice == "paper") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else if (botChoice == "spock" && userChoice == "scissors") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.lose, {choice: botChoice, user: chat.username}), 30e3);
+                                bot.skipDJ();
+                            
+                            } else if (botChoice == "spock" && userChoice == "lizard") {
+                                return  bot.sendChat(utils.replaceString(lang.rps.win, {choice: botChoice, user: chat.username}), 30e3);
+                            
+                            } else {
+                                return  bot.sendChat(utils.replaceString(lang.rps.error, {user: chat.username}), 30e3);
+                            }
 
 				
 		}
